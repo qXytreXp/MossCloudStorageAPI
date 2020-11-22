f9d7bd3901a2d9bbc99cdca3c2d4ac43de922830
IllyaMoss <50335232+qXytreXp@users.noreply.github.com>
2020-11-22 21:11:22 +0200

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -7,7 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('storage/', views.DocumentListView.as_view()),
     path('add-doc/', views.AddDocumentView.as_view()),
-    path('register/', views.UserRegisterView.as_view()),
     path('download-doc/<str:username>/<str:token>/<str:filename>/', views.DownloadDocView.as_view()),
     path('delete-doc/', views.DeleteDocumentView.as_view())
 
